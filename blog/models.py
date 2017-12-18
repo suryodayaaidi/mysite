c585208526aa045a614b8666b15027218d4d883a
suryodayaaidi <suryodayaaidi@gmail.com>
2017-12-18 11:12:32 +0545

    first commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    comment = models.TextField()
+    post_id = models.IntegerField(default=0)    
+    
